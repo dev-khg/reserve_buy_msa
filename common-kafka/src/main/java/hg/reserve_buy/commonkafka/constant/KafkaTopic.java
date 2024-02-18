cdeaafa0ae01a4fa2e1e3c8b7c0159ffc303858d
HongGeun <ghdrms163@gmail.com>
2024-02-19 02:48:54 +0900

    feat: stock-service 카프카메세지 적용

--- common-kafka/src/main/java/hg/reserve_buy/commonkafka/constant/KafkaTopic.java
+++ common-kafka/src/main/java/hg/reserve_buy/commonkafka/constant/KafkaTopic.java
@@ -1,4 +1,10 @@
 package hg.reserve_buy.commonkafka.constant;
 
-public class KafkaTopic {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class KafkaTopic {
+    public final static String ORDER_PAYED = "ORDER_PAYED";
+    public final static String ORDER_CANCELED = "ORDER_CANCELED";
 }
