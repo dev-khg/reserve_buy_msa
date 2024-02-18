f8597f2be6f5c039463fa9574a5465a286244ae5
HongGeun <ghdrms163@gmail.com>
2024-02-18 18:12:01 +0900

    init-item service

---
+++ item-service-api/src/main/java/hg/reserve_buy/itemserviceapi/ItemServiceApiApplication.java
@@ -0,0 +1,13 @@
+package hg.reserve_buy.itemserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApiApplication.class, args);
+    }
+
+}
---
+++ item-service-api/src/test/java/hg/reserve_buy/itemserviceapi/ItemServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.reserve_buy.itemserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
