a3239e4d540aa32af57a6a7ca46c54ab95f0e054
HongGeun <ghdrms163@gmail.com>
2024-02-18 20:39:19 +0900

    feat: eureka 서버 등록

---
+++ eureka-service/src/test/java/hg/reserve_buy/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.reserve_buy.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
