273624ba172ce6cab4bd4225be28e711a8261aa2
HongGeun <ghdrms163@gmail.com>
2024-02-20 02:35:59 +0900

    feat: 선착순 전용 캐싱 모듈 추가

--- common-redis/src/main/java/hg/reserve_buy/commonredis/timedeal/RedisTimeDealCacheConfig.java
+++ common-redis/src/main/java/hg/reserve_buy/commonredis/timedeal/RedisTimeDealCacheConfig.java
@@ -1,4 +1,4 @@
-package hg.reserve_buy.commonredis.price;
+package hg.reserve_buy.commonredis.timedeal;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,28 +7,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisPriceCacheConfig {
-    @Value("${redis.price.host}")
+public class RedisTimeDealCacheConfig {
+    @Value("${redis.time-deal.host}")
     private String host;
-    @Value("${redis.price.port}")
+    @Value("${redis.time-deal.port}")
     private int port;
 
     @Bean
-    public RedisConnectionFactory redisPriceConnectionFactory() {
+    public RedisConnectionFactory redisTimeDealConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
-    public RedisTemplate<String, Integer> priceRedisTemplate(
-            @Qualifier("redisPriceConnectionFactory") RedisConnectionFactory factory) {
-        final RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> timeDealRedisTemplate(
+            @Qualifier("redisTimeDealConnectionFactory") RedisConnectionFactory factory) {
+        final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
     }
---
+++ common-redis/src/main/java/hg/reserve_buy/commonredis/timedeal/RedisTimeDealScripts.java
@@ -0,0 +1,20 @@
+package hg.reserve_buy.commonredis.timedeal;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RedisTimeDealScripts {
+    private final static String timeDealOrderScript  = "local storedValue = redis.call('GET', KEYS[1]) " +
+            "if storedValue and tonumber(storedValue) > tonumber(ARGV[1]) then " +
+            "    redis.call('SET', KEYS[1], tonumber(storedValue) - tonumber(ARGV[1])) " +
+            "    return true " +
+            "else " +
+            "    return false " +
+            "end";
+
+    public static RedisScript<Boolean> reserveOrderScript
+            = new DefaultRedisScript<>(timeDealOrderScript, Boolean.class);
+}
