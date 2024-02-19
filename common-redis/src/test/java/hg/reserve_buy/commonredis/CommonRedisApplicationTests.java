3be98f7cde9b3941be325ae5d582f308edf4f6d2
HongGeun <ghdrms163@gmail.com>
2024-02-19 15:24:29 +0900

    feat: 레디스 모듈 분리

---
+++ common-redis/src/test/java/hg/reserve_buy/commonredis/CommonRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.reserve_buy.commonredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
