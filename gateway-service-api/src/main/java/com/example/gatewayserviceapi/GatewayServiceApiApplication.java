fb0c86236953dcbd56025104488b60249d46d905
HongGeun <ghdrms163@gmail.com>
2024-02-20 21:55:45 +0900

    init gateway service

---
+++ gateway-service-api/src/main/java/com/example/gatewayserviceapi/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApiApplication.class, args);
+    }
+
+}
---
+++ gateway-service-api/src/test/java/com/example/gatewayserviceapi/GatewayServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
