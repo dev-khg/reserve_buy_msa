6d22f334cb0580b83805dd3bb4683f7761720e51
HongGeun <ghdrms163@gmail.com>
2024-02-21 21:59:25 +0900

    feat: dockerize 도커 설정 완료

--- common-kafka/src/main/java/hg/reserve_buy/commonkafka/config/KafkaProducer.java
+++ common-kafka/src/main/java/hg/reserve_buy/commonkafka/config/KafkaProducer.java
@@ -1,17 +1,21 @@
 package hg.reserve_buy.commonkafka.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 
+@Slf4j
 @RequiredArgsConstructor
 public abstract class KafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void publish(String topic, Object data) {
+        log.info("======> KAFKA TOPIC [{}], Data[{}]", topic, data);
         kafkaTemplate.send(topic, data);
     }
 
     public void publish(String key, String topic, Object data) {
+        log.info("======> KAFKA TOPIC [{}]  Data[{}] Key [{}]", topic, data, key);
         kafkaTemplate.send(topic, key, data);
     }
 }
