36bd384f010569c7c9162186a64d6b06658a3d72
HongGeun <ghdrms163@gmail.com>
2024-02-26 02:30:52 +0900

    feat: added virtual thread 적용 재고 서비스

---
+++ stock-service-api-with-virtual-thread/src/main/java/hg/reverse_buy/stockserviceapiwithvirtualthread/StockServiceApiWithVirtualThreadApplication.java
@@ -0,0 +1,13 @@
+package hg.reverse_buy.stockserviceapiwithvirtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApiWithVirtualThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApiWithVirtualThreadApplication.class, args);
+    }
+
+}
---
+++ stock-service-api-with-virtual-thread/src/test/java/hg/reverse_buy/stockserviceapiwithvirtualthread/StockServiceApiWithVirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.reverse_buy.stockserviceapiwithvirtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApiWithVirtualThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
